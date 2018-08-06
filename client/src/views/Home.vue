a989849202f9eefad7b6dd70ca9a77034c777dd5
hacktiv8 <hacktiv8@H8JKT029.localdomain>
2018-08-06 19:04:31 +0700

    add form register

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,96 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  
+    <v-app id="inspire">
+    <v-jumbotron :gradient="gradient" style="height:100vh">
+      <v-container  >
+          <v-layout row>
+              <v-flex xs12 sm6 offset-sm3>
+                  <v-card>
+                    <v-container >
+                        <form>
+                            <v-layout row>
+                            <v-flex xs12>
+                              <v-text-field name ="email" label="name" v-model="inputName" type="name">
+                                {{user}}
+                              </v-text-field>
+                            </v-flex>
+                          </v-layout>
+                          <v-layout row>
+                            <v-flex xs12>
+                              <v-text-field name ="email" label="email" v-model="inputEmail" type="name" >
+                              </v-text-field>
+                            </v-flex>
+                          </v-layout>
+                           <v-layout row>
+                            <v-flex xs12>
+                              <v-text-field  label="password"  v-model="inputPassword" type="password">
+                              </v-text-field>
+                            </v-flex>
+                          </v-layout>
+                             <p class="text-lg-right">Login</p>
+                                <v-layout row>
+                               
+                                <v-flex xs 12>
+                                    <div>
+                                   <v-btn outline color="indigo">Register</v-btn>
+                                </div>
+                                
+                                  </v-flex>
+                                </v-layout>      
+                        </form>
+                    </v-container>
+                  </v-card>
+              </v-flex>
+          </v-layout>
+      </v-container>
+  </v-jumbotron>
+ </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      gradient : 'to right, #076585,#fff'
+    }
   }
 }
 </script>
+
+
+
+
+<style lang="scss" scoped>
+
+.v-card {
+    display: block;
+    border-radius: 5px;
+    margin-top : 250px;
+    border: solid 2px;  
+    min-width: 0;
+    position: relative;
+    text-decoration: none;
+    -webkit-box-shadow: 0 2px 1px -1px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 3px 0 rgba(0,0,0,.12);
+    box-shadow: 5px 5px;
+.application--wrap {
+    -webkit-box-flex: 1;
+    -ms-flex: 1 1 auto;
+    flex: 1 1 auto;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    height: 0px;
+    max-width: 100%;
+    position: relative;
+}
+}
+</style>
+
+
+
